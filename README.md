beed430066e3a323dcb948e440c0c91a29945a8d
Michael Cassano <michael.cassano@kennasecurity.com>
2023-11-17 11:01:11 -0700

    basic playground

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# gpt-risk-meters-hackathon
+Using python 3.9.6, install openai
+```
+pip install openai
+```
+
+Set your environment variables
+```
+export AZURE_OPENAI_KEY=<MIKE HAS THE KEY>
+export AZURE_OPENAI_ENDPOINT=https://cloudsec-hackathon-apim.azure-api.net/
+```
+
+Using the text in system.prompt and user.prompt, run the chat completion:
+```
+python ./testchat.py
+```
---
+++ testchat.py
@@ -0,0 +1,24 @@
+import os
+from openai import AzureOpenAI
+
+client = AzureOpenAI(
+  azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"), 
+  api_key=os.getenv("AZURE_OPENAI_KEY"),  
+  api_version="2023-05-15"
+)
+
+with open("system.prompt") as f:
+    system_prompt = f.read()
+
+with open("user.prompt") as f:
+    user_prompt = f.read()
+
+response = client.chat.completions.create(
+    model="hack-gpt-35", # model = "deployment_name".
+    messages=[
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt}
+    ]
+)
+
+print(response.choices[0].message.content)
