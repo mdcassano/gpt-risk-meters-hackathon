5c22819f400262c98af41aecb76989eb3052f7e7
Michael Cassano <michael.cassano@kennasecurity.com>
2023-11-21 10:08:36 -0700

    refactor prompt loads and locator definition

--- testchat.py
+++ testchat.py
@@ -2,21 +2,30 @@ import os
 import sys
 from openai import AzureOpenAI
 
+def load_prompts_from_file_system():
+    prompt_prefix = ""
+    if len(sys.argv) > 1:
+        prompt_prefix = sys.argv[1] + "-"
+    
+    with open("base-locator-description.prompt") as f:
+        base_prompt = f.read()
+    
+    with open("%ssystem.prompt" % (prompt_prefix)) as f:
+        system_prompt = f.read()
+    
+    system_prompt = base_prompt + "\n\n" + system_prompt
+    
+    with open("%suser.prompt" % (prompt_prefix)) as f:
+        user_prompt = f.read()
+    return system_prompt, user_prompt
+
 client = AzureOpenAI(
   azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"), 
   api_key=os.getenv("AZURE_OPENAI_KEY"),  
-  api_version="2023-05-15"
+  api_version="2023-05-15" # TODO: is this a good date?
 )
 
-prompt_prefix = ""
-if len(sys.argv) > 1:
-    prompt_prefix = sys.argv[1] + "-"
-
-with open("%ssystem.prompt" % (prompt_prefix)) as f:
-    system_prompt = f.read()
-
-with open("%suser.prompt" % (prompt_prefix)) as f:
-    user_prompt = f.read()
+system_prompt, user_prompt = load_prompts_from_file_system()
 
 response = client.chat.completions.create(
     model="hack-gpt-4",
