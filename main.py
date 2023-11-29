321c07b965f8bb8174a595fe362743dd9e1e2cb5
Abhishek <ababhis2@cisco.com>
2023-11-30 03:43:29 +0530

    Fix lint

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['https://testkenna.kdev.docker'],
+    allow_origins=["https://testkenna.kdev.docker"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
